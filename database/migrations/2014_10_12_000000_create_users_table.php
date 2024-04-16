ad8d93acdf6d710de9a32a952ff7c5e8b51333fb
Aryasaleh19 <aryasaleh582@gmail.com>
2024-04-16 23:40:22 +0800

    frist commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,15 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id("id")->primaryKey();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('No_Telp')->nullable();
+            $table->integer('saldo');
+            $table->integer('p_code');
+            $table->integer('invite_code')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
